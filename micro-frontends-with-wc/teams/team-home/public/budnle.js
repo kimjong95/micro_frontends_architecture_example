9e34c4729cbf4300fc2fb1ab782e0786a6f5c462
jhkim <jhkim@nextree.io>
2024-01-26 08:34:18 +0900

    web-component 통합 예제 진행중

---
+++ micro-frontends-with-wc/teams/team-home/public/budnle.js
@@ -0,0 +1,29 @@
+class HomeComponent extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+
+    const template = document.createElement("template");
+
+    template.innerHTML = `
+      <style>
+        h2 {
+          color: blue;
+        }
+      </style>
+      <h2>홈</h2>
+      <button id="btn>버튼</button>
+    `;
+
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot
+      .querySelector("#btn")
+      .addEventListener("click", this.onClick);
+  }
+
+  onClick() {
+    alert("버튼이 클릭됨");
+  }
+}
+
+customElements.define("home-component", HomeComponent);
